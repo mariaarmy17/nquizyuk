85574469ce00b071c8682379cf0da61976a585b5
Nama Kamu <emailkamu@example.com>
2026-06-23 00:40:31 +0800

    Fix static file serving order for Railway

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve static frontend files
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'src')));
 
