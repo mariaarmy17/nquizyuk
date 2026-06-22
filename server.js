a7349fbe0e825b92d08d14c15adc73d83a1be93e
Nama Kamu <emailkamu@example.com>
2026-06-23 00:11:14 +0800

    Fix Railway sqlite3 build and Express catch-all route

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.post('/api/answers', (req, res) => {
 });
 
 // Serve index.html untuk route tidak dikenal (SPA support)
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
